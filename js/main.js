7f00f41787c62879d53f6cb8de0427954634854d
Eduardo Visa <edu.mvis29@outlook.com>
2022-06-02 14:17:51 -0500

    Se agregaron validaciones y funcionalidades requeridas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let contador = 1;
+let costoTotal = 0;
 let element = document.getElementById("totalPrecio");
 element.innerHTML = "Total en precio";
 
@@ -5,6 +7,8 @@ let txtNombre = document.getElementById("name");
 // txtNombre.value = "Leche Semidescremada";
 // console.log(txtNombre.value);
 let txtNumber = document.getElementById("Number");
+
+let total = document.getElementById("precioTotal");
 
 // let campos = document.getElementsByClassName("campo");
 // // console.log(campos.length);
@@ -33,27 +37,94 @@ let cuerpoTabla = tabla.getElementsByTagName("tbody");
 //     </tr>
 // `;
 
-let agregar = document.getElementById("btnAgregar");
 // console.log(agregar);
 // agregar.onclick = 
 
-let indice = 1;
-agregar.addEventListener("click", (event) => {
-    // console.log("click en el botón agregar", event);
-    let precio = Math.random() * 50;
-    tmp = `
-        <tr>
-            <th scope="row">${indice}</th>
-            <td>${txtNombre.value}</td>
-            <td>${txtNumber.value}</td>
-            <td>$ ${precio}</td>
-        </tr>`;
-        cuerpoTabla[0].innerHTML += tmp;
-        txtNombre.value = "";
-        txtNumber.value = "";
-        txtNombre.focus();
+function validarNombre() {
+    if (txtNombre.value.length < 3) {
+        return false;
+    } // if
+    return true; 
+} // validación
 
-        indice++;
+function validarCantidad(){
+    if(txtNumber.value.length==0) {
+        return false;
+    }// if
+    if (isNaN(txtNumber.value)){
+        return false;
+    }//if
+
+    if (parseFloat(txtNumber.value)<=0) {
+        return false;
+    }//if
+    return true;
+}// validarCantidad
+
+
+let agregar = document.getElementById("btnAgregar");
+
+agregar.addEventListener("click", (event) => {
+    event.preventDefault();
+    if ((! validarNombre()) || (! validarCantidad())) {
+        document.getElementById("alertValidaciones").style.display = "block";
+        let lista = "";
+        if (! validarNombre()) {
+            txtNombre.style.border = "red thin solid";
+            lista += "<li>Se debe de escribir un nombre válido.</li>";
+        } else {
+            txtNombre.style.border = "";
+        } // if
+        if (! validarCantidad()) {
+            txtNumber.style.border = "red thin solid";
+            lista += "<li>Se debe de escribir una cantidad válida.</li>";
+        } else {
+            txtNumber.style.border = "";
+        } // if
+        document.getElementById("alertValidaciones").innerHTML = `Los campos deben de ser llenados correctamente.
+        <ul>${lista}</ul>`;
+
+        setTimeout (function(){
+            document.getElementById("alertValidaciones").style.display = "none";
+        }, 
+            5000
+        );
+
+        return false;
+    } // if
+
+    txtNombre.style.border = "";
+    txtNumber.style.border = "";
+
+    document.getElementById("alertValidaciones").style.display = "none";
+    let precio = (Math.floor((Math.random() * 50) * 100)) / 100;
+    costoTotal += parseFloat(txtNumber.value) * precio;
+    tmp = `
+    <tr>
+    <th scope="row">${contador}</th>
+    <td>${txtNombre.value}</td>
+    <td>${txtNumber.value}</td>
+    <td>$ ${precio}</td>
+    </tr>`;
+    document.getElementById("contadorProductos").innerHTML = contador;
+    cuerpoTabla[0].innerHTML += tmp;
+    total.innerHTML = `$ ${costoTotal.toFixed(2)}`;
+    txtNombre.value = "";
+    txtNumber.value = "";
+    txtNombre.focus();
+    contador++;   
+
+    // console.log("click en el botón agregar", event);
     // console.log(txtNombre.value, txtNumber.value);
     // console.log(tmp);
+});
+
+// evento cuando s epierde el focus
+txtNombre.addEventListener("blur", (event) => {
+    // el target hace referencia al txtNombre
+    event.target.value = event.target.value.trim();
+});
+
+txtNumber.addEventListener("blur", (event) => {
+    event.target.value = event.target.value.trim();
 });
