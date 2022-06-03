9babe8fcd4ef29ecbe85c8cb3664cfe994d3ebc8
Eduardo Visa <edu.mvis29@outlook.com>
2022-06-03 12:20:35 -0500

    Se agregaron los elementos de la tabla al localStorage y se leen cuando carga la página

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-let contador = 1;
+let contador = 0;
 let costoTotal = 0;
 let totalEnProductos = 0;
+
+// Arreglo global para almacenar la lista de compras
+let datos = [];
+
 let element = document.getElementById("totalPrecio");
 element.innerHTML = "Total en precio";
 
@@ -94,6 +98,8 @@ agregar.addEventListener("click", (event) => {
         return false;
     } // if
 
+    contador++;   
+
     txtNombre.style.border = "";
     txtNumber.style.border = "";
     document.getElementById("alertValidaciones").style.display = "none";
@@ -102,6 +108,24 @@ agregar.addEventListener("click", (event) => {
     let cantidad = parseFloat(txtNumber.value);
     totalEnProductos += (cantidad<1)? Math.ceil(cantidad):parseInt(cantidad);
     
+    // window.localStorage
+    localStorage.setItem("contadorProductos", contador);
+    localStorage.setItem("totalProductos", totalEnProductos);
+    localStorage.setItem("precioTotal", costoTotal.toFixed(2));
+
+    // JSON
+    let elemento = `{
+        "id" : ${contador}, 
+        "nombre" : "${txtNombre.value}", 
+        "cantidad" : ${txtNumber.value}, 
+        "precio" : ${precio}
+    }`
+
+    // parse = toma una cadena y lo convierte a un arreglo de objeto
+    datos.push(JSON.parse(elemento));
+    localStorage.setItem("elementosTabla", JSON.stringify(datos));
+    // console.log(datos);
+
     tmp = `
     <tr>
     <th scope="row">${contador}</th>
@@ -114,15 +138,9 @@ agregar.addEventListener("click", (event) => {
     document.getElementById("totalProductos").innerHTML = totalEnProductos;
     total.innerHTML = `$ ${costoTotal.toFixed(2)}`;
 
-    // window.localStorage
-    localStorage.setItem("contadorProductos", contador);
-    localStorage.setItem("totalProductos", totalEnProductos);
-    localStorage.setItem("precioTotal", costoTotal.toFixed(2));
-
     txtNombre.value = "";
     txtNumber.value = "";
     txtNombre.focus();
-    contador++;   
 
     // console.log("click en el botón agregar", event);
     // console.log(txtNombre.value, txtNumber.value);
@@ -152,4 +170,18 @@ window.addEventListener("load", function() {
         costoTotal = parseFloat(localStorage.getItem("precioTotal"));
         document.getElementById("precioTotal").innerHTML = `$ ${costoTotal.toFixed(2)}`;
     } // if precioTotal
+
+    if (localStorage.getItem("elementosTabla") != null) {
+        datos = JSON.parse(localStorage.getItem("elementosTabla"));
+        datos.forEach(element => {
+            cuerpoTabla[0].innerHTML += `
+                <tr>
+                    <th scope="row">${element.id}</th>
+                    <td>${element.nombre}</td>
+                    <td>${element.cantidad}</td>
+                    <td>$ ${element.precio}</td>
+                </tr>`;
+        });
+    } // if elementosTabla
+
 });
